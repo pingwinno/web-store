28fd8da46d93f0ca3ce9e651ada98b86dc2ba145
mromanc <you@example.com>
2021-11-18 06:02:55 -0800

    Add service layer Add basic XSS protection

---
+++ src/main/java/com/study/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.study.service;
+
+import com.study.exception.NotFoundException;
+import com.study.model.Product;
+import com.study.persistance.ProductRepository;
+import com.study.util.ProductUtil;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(long id) {
+        return productRepository.findById(id)
+                                .orElseThrow(() -> new NotFoundException(MessageFormat
+                                        .format("Product with id {0} not found", id)));
+    }
+
+    public void createProduct(Product product) {
+        ProductUtil.escapeProduct(product);
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Product product) {
+        ProductUtil.escapeProduct(product);
+        productRepository.update(product);
+    }
+}
